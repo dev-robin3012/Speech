b88ed1df1b86e418b918a6b13f61832f00e5c707
Shahadat_Robin <sh.robin025@gmail.com>
2022-09-10 00:49:02 +0600

    input box done

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,39 +1,34 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
 import styles from './input.module.scss';
 
 const Input = ({ label, name, placeholder, type, prefix, ...rest }) => {
   const [showPassword, setShowPassword] = useState(false);
-  const [value, setValue] = useState('');
+  const inputRef = useRef();
 
   return (
-    <div className={styles.input_container}>
-      <div className={styles.input_group}>
-        {prefix && prefix}
-
-        <input
-          type={showPassword ? 'text' : type}
-          placeholder={placeholder}
-          name={name}
-          onChange={(e) => setValue(e.target.value)}
-          {...rest}
-        />
-
-        {type === 'password' &&
-          (!showPassword ? (
-            <AiOutlineEye
-              className={styles.showPassword}
-              onClick={() => setShowPassword(true)}
-            />
-          ) : (
-            <AiOutlineEyeInvisible
-              className={styles.hidePassword}
-              onClick={() => setShowPassword(false)}
-            />
-          ))}
-      </div>
-
-      <label htmlFor={name} className={styles.label_in}>
+    <div className={styles.input_group}>
+      {prefix && prefix}
+      <input
+        ref={inputRef}
+        type={showPassword ? 'text' : type}
+        placeholder={placeholder}
+        name={name}
+        {...rest}
+      />
+      {type === 'password' &&
+        (!showPassword ? (
+          <AiOutlineEye
+            className={styles.showPassword}
+            onClick={() => setShowPassword(true)}
+          />
+        ) : (
+          <AiOutlineEyeInvisible
+            className={styles.hidePassword}
+            onClick={() => setShowPassword(false)}
+          />
+        ))}
+      <label htmlFor={name} onClick={(e) => inputRef.current.focus()}>
         {label}
       </label>
     </div>
