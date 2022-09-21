0ab788c72afffd59fb5a4eb56ab9377a10973db3
Shahadat_Robin <sh.robin025@gmail.com>
2022-09-22 00:21:06 +0600

    smtp connected

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,47 +1,58 @@
+import { createTransport } from 'nodemailer';
 import dbConnection from '../../lib/db.connection';
 import User from '../../model/user.model';
+
+const transporter = createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  secure: false,
+  auth: {
+    user: process.env.smtp_email,
+    pass: process.env.smtp_pass,
+  },
+});
 
 const handleSignup = async (req, res) => {
   await dbConnection();
 
   try {
-    const user = await User.findOne({ phone: req.body.email });
+    const user = await User.findOne({ email: req.body.email });
 
     if (user)
       return res
         .status(409)
         .send({ message: 'This email is already registered.' });
 
-    console.log(req.body);
+    const message = {
+      from: `"Speech" <${process.env.smtp_email}>`,
+      to: 'sh.robin025@gmail.com',
+      subject: 'Verify User',
+      html: `<div style="font-size:18px;">
+      <p>Hi ${req.body.name}!</p>
 
-    // console.log({ user });
-    // if (user) return res.status(200).send({ credentials: user });
+      <p>Your verification code is</p>
+      <p style="font-size:25px;">456987</p>
 
-    // await vonage.verify.request(
-    //   { number: req.body.phone, brand: 'Vonage' },
-    //   (err, result) => {
-    //     if (err) {
-    //       console.error('sending err:', err);
-    //     } else {
-    //       const verifyRequestId = result.request_id;
-    //       console.log('request_id', verifyRequestId);
-    //     }
-    //   }
-    // );
+      <p>
+        Enter this code in our [website or app] to activate your account.
+      </p>
 
-    // const response = await courier.send({
-    //   message: {
-    //     to: {
-    //       phone_number: req.body.phone,
-    //     },
-    //     template: 'NHG40XN35K4DG9H2PS4MCS7WQMJG',
-    //     data: {
-    //       recipientName: 'Shahadat Robin',
-    //     },
-    //   },
-    // });
+      <p>Click here [open code in app] to open the [app/portal landing page].</p>
 
-    // console.log(response);
+      <p>
+        If you have any questions, send us an email to support team.
+      </p>
+      
+      <p style="margin:0;">We’re glad you’re here!</p>
+
+      <br/>
+      <p>The Speech team</p>
+      </div>`,
+    };
+
+    const result = await transporter.sendMail(message);
+
+    console.log(result);
 
     return res.status(201).send({ message: 'User Created Successfully.' });
   } catch (error) {
